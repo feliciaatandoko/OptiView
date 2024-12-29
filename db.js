85c8c54a8b30730599a6f5ec224e7335df066ec8
Felicia Andrea T <feliciaatandoko@gmail.com>
2024-12-29 23:53:36 +0700

    Update db.js

--- db.js
+++ db.js
@@ -5,7 +5,7 @@ const connectDB = () => {
     const db = mysql.createConnection({
         host: process.env.DB_HOST || 'localhost',
         user: process.env.DB_USER || 'root',
-        password: process.env.DB_PASSWORD || 'feliiB!nu$13',
+        password: process.env.DB_PASSWORD || '',
         database: process.env.DB_NAME || 'optiview',
     });
 
